b5f6e38a7fe106256db478be817cddff3a9ce6fb
Prince26122002 <prince.mittal.cse.2020@miet.ac.in>
2024-10-04 12:02:49 +0530

    add authorization in report controller

--- src/main/java/sfa/order_service/controller/ReportController.java
+++ src/main/java/sfa/order_service/controller/ReportController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import sfa.order_service.constant.UserRole;
 import sfa.order_service.dto.request.ReportsRequest;
 import sfa.order_service.dto.response.ReportsResponse;
 import sfa.order_service.enums.SalesLevel;
@@ -24,7 +25,7 @@ public class ReportController {
     private final ReportServices reportServices;
 
     @GetMapping("/sales")
-//    @UserAuthorization
+    @UserAuthorization(allowedRoles = {UserRole.Create_Manager, UserRole.Edit_Manager, UserRole.Delete_Manager,UserRole.View_Manager,UserRole.Manager})
     public ResponseEntity<ReportsResponse> getSalesReport(@RequestParam LocalDateTime startDate, @RequestParam LocalDateTime endDate, @RequestParam SalesLevel salesLevel) throws ParseException {
         ReportsRequest reportsRequest = new ReportsRequest(startDate, endDate, salesLevel);
         ReportsResponse reportsResponse = reportServices.getSalesReportBetweenDatesAndSalesLevel(reportsRequest);
