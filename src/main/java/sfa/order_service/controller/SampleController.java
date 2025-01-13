89cb734c2df0e12e80ffd9b86220043d5eb32dd0
Vivek Pandey <vivekpandey1464@gmail.com>
2025-01-13 13:58:47 +0530

    url cjanges in rest call

--- src/main/java/sfa/order_service/controller/SampleController.java
+++ src/main/java/sfa/order_service/controller/SampleController.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import sfa.order_service.constant.UserRole;
 import sfa.order_service.dto.request.SampleReq;
 import sfa.order_service.dto.response.PaginatedResp;
 import sfa.order_service.dto.response.SampleRes;
+import sfa.order_service.interceptor.UserAuthorization;
 import sfa.order_service.service.SampleServices;
 
 import java.util.List;
@@ -19,6 +21,7 @@ public class SampleController {
     private final SampleServices sampleServices;
 
     @PostMapping
+    @UserAuthorization(allowedRoles = {UserRole.ClientFMCG,UserRole.Create_Manager, UserRole.Edit_Manager, UserRole.Delete_Manager, UserRole.View_Manager, UserRole.Manager, UserRole.Reporting_Manager, UserRole.Super_Admin})
     public ResponseEntity<SampleRes> createSample(@RequestBody SampleReq sampleReq) {
         log.info("Creating a sample: {}", sampleReq);
         SampleRes response = sampleServices.createSample(sampleReq);
@@ -26,6 +29,7 @@ public class SampleController {
     }
 
     @PutMapping("/{id}")
+    @UserAuthorization(allowedRoles = {UserRole.ClientFMCG,UserRole.Create_Manager, UserRole.Edit_Manager, UserRole.Delete_Manager, UserRole.View_Manager, UserRole.Manager, UserRole.Reporting_Manager, UserRole.Super_Admin})
     public ResponseEntity<SampleRes> updateSample(@PathVariable Long id, @RequestBody SampleReq sampleReq) {
         log.info("Updating sample with ID: {}", id);
         SampleRes response = sampleServices.updateSample(id, sampleReq);
@@ -33,6 +37,7 @@ public class SampleController {
     }
 
     @GetMapping("/{id}")
+    @UserAuthorization(allowedRoles = {UserRole.ClientFMCG,UserRole.Create_Manager, UserRole.Edit_Manager, UserRole.Delete_Manager, UserRole.View_Manager, UserRole.Manager, UserRole.Reporting_Manager, UserRole.Super_Admin})
     public ResponseEntity<SampleRes> getSampleById(@PathVariable Long id) {
         log.info("Fetching sample with ID: {}", id);
         SampleRes response = sampleServices.getSampleById(id);
@@ -40,6 +45,7 @@ public class SampleController {
     }
 
     @DeleteMapping("/{id}")
+    @UserAuthorization(allowedRoles = {UserRole.ClientFMCG,UserRole.Create_Manager, UserRole.Edit_Manager, UserRole.Delete_Manager, UserRole.View_Manager, UserRole.Manager, UserRole.Reporting_Manager, UserRole.Super_Admin})
     public ResponseEntity<Void> deleteSample(@PathVariable Long id) {
         log.info("Deleting sample with ID: {}", id);
         sampleServices.deleteSample(id);
@@ -47,6 +53,7 @@ public class SampleController {
     }
 
     @PostMapping("/bulk")
+    @UserAuthorization(allowedRoles = {UserRole.ClientFMCG,UserRole.Create_Manager, UserRole.Edit_Manager, UserRole.Delete_Manager, UserRole.View_Manager, UserRole.Manager, UserRole.Reporting_Manager, UserRole.Super_Admin})
     public ResponseEntity<List<SampleRes>> createSamplesInBulk(@RequestBody List<SampleReq> sampleReqList) {
         log.info("Creating samples in bulk: {}", sampleReqList.size());
         List<SampleRes> response = sampleServices.createSampleInBulk(sampleReqList);
@@ -54,6 +61,7 @@ public class SampleController {
     }
 
     @GetMapping("/manager/{managerId}")
+    @UserAuthorization(allowedRoles = {UserRole.ClientFMCG,UserRole.Create_Manager, UserRole.Edit_Manager, UserRole.Delete_Manager, UserRole.View_Manager, UserRole.Manager, UserRole.Reporting_Manager, UserRole.Super_Admin})
     public ResponseEntity<PaginatedResp<SampleRes>> getSamplesByManager(
             @PathVariable Long managerId,
             @RequestParam(defaultValue = "0") int page,
@@ -66,6 +74,7 @@ public class SampleController {
     }
 
     @GetMapping("/member/{memberId}")
+    @UserAuthorization(allowedRoles = {UserRole.ClientFMCG,UserRole.Create_Manager, UserRole.Edit_Manager, UserRole.Delete_Manager, UserRole.View_Manager, UserRole.Manager, UserRole.Reporting_Manager, UserRole.Super_Admin})
     public ResponseEntity<PaginatedResp<SampleRes>> getSamplesByMember(
             @PathVariable Long memberId,
             @RequestParam(defaultValue = "0") int page,
@@ -78,6 +87,7 @@ public class SampleController {
     }
 
     @GetMapping
+    @UserAuthorization(allowedRoles = {UserRole.ClientFMCG,UserRole.Create_Manager, UserRole.Edit_Manager, UserRole.Delete_Manager, UserRole.View_Manager, UserRole.Manager, UserRole.Reporting_Manager, UserRole.Super_Admin})
     public ResponseEntity<PaginatedResp<SampleRes>> getAllSamples(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int pageSize,
