003feedeeb3409da3347e2fedc6cc4df446bf069
Vivek Pandey <vivekpandey1464@gmail.com>
2025-01-30 00:32:46 +0530

    coupom changes

--- src/main/java/sfa/order_service/repo/OrderRepository.java
+++ src/main/java/sfa/order_service/repo/OrderRepository.java
@@ -19,32 +19,11 @@ import java.util.Set;
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
    Page<OrderEntity> findById(Long orderId, Pageable pageable);
-   @Query("SELECT o.invoiceNumber FROM OrderEntity o " +
-           "WHERE o.clientFmcgId = :clientFmcgId AND o.salesLevel = :salesLevel " +
-           "GROUP BY o.invoiceNumber")
-   Page<String> findDistinctInvoiceNumbers(
-           @Param("clientFmcgId") Long clientFmcgId,
-           @Param("salesLevel") SalesLevel salesLevel,
-           Pageable pageable
-   );
-
-   @Query("SELECT o.invoiceNumber FROM OrderEntity o " +
-           "WHERE o.salesLevel = :salesLevel " +
-           "AND o.memberId IN :memberIds " +
-           "GROUP BY o.invoiceNumber")
-   Page<String> findDistinctInvoiceNumbersByReportingManagerId(
-           @Param("salesLevel") SalesLevel salesLevel,
-           @Param("memberIds") Set<Long> memberIds,
-           Pageable pageable
-   );
-
-
 
    @Query("SELECT o FROM OrderEntity o WHERE o.invoiceNumber = :invoiceNumber")
    List<OrderEntity> findOrdersByInvoiceNumber(@Param("invoiceNumber") String invoiceNumber);
 
    List<OrderEntity> findByProductId(Long productId);
-   Page<OrderEntity> findByOrderCallStatus(OrderCallStatus orderCallStatus, Pageable pageable);
    @Query("SELECT o FROM OrderEntity o WHERE o.createdDate BETWEEN :startDate AND :endDate AND o.salesLevel = :salesLevel")
    List<OrderEntity> findAllByCreatedDateBetweenAndSalesLevel(@Param("startDate") LocalDateTime startDate,
                                                               @Param("endDate") LocalDateTime endDate,
