0cfecca93b4bfc672f90835371d86675e9cf7009
Vivek Pandey <vivekpandey1464@gmail.com>
2025-03-28 15:41:26 +0530

    getLastTenDaysOrderByOutletIdAndMemberId

--- src/main/java/sfa/order_service/Configs/CorsConfig.java
+++ src/main/java/sfa/order_service/Configs/CorsConfig.java
@@ -9,17 +9,24 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*")
-                .allowedOrigins("https://www.prism-sfa-dev.net", "https://api.prism-sfa-dev.net", "http://localhost:3000","https://dev.prism-sfa-dev.net","https://staging.prism-sfa-dev.net/",
-                        "http://staging.prism-sfa-dev.net/",
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://www.prism-sfa-dev.net",
+                        "https://api.prism-sfa-dev.net",
+                        "http://localhost:3000",
+                        "https://dev.prism-sfa-dev.net",
+                        "https://staging.prism-sfa-dev.net",
+                        "http://staging.prism-sfa-dev.net",
                         "http://88.222.215.37:9092",
                         "https://88.222.215.37:9092",
                         "http://88.222.215.37:9092/order-service",
                         "https://88.222.215.37:9092/order-service",
                         "http://88.222.215.37",
-                        "https://www.dev.prism-sfa-dev.net")
+                        "https://www.dev.prism-sfa-dev.net"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*").allowCredentials(true)
+                .allowedHeaders("*")
+                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
