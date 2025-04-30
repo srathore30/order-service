bc32f72160722d3aa66746b3ad16a0f84f7b681d
Vivek Pandey <vivekpandey1464@gmail.com>
2025-04-30 18:11:01 +0530

    apporval status get all api created

--- src/main/java/sfa/order_service/repo/TransactionRepository.java
+++ src/main/java/sfa/order_service/repo/TransactionRepository.java
@@ -1,7 +1,11 @@
 package sfa.order_service.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import sfa.order_service.entity.TransactionEntity;
 
 import java.util.List;
@@ -10,8 +14,11 @@ import java.util.List;
 public interface TransactionRepository extends JpaRepository<TransactionEntity, Long> {
 //   Page<OrderEntity> findById(Long orderId, Pageable pageable);
    List<TransactionEntity> findByClientId(Long clientId);
-   void deleteAllByOrderId(List<Long> ids);
 
+   @Modifying
+   @Transactional
+   @Query("DELETE FROM TransactionEntity t WHERE t.orderId IN :ids")
+   void deleteAllByOrderId(@Param("ids") List<Long> ids);
    TransactionEntity findByClientIdAndOrderId(Long clientId, Long orderId);
 
 }
