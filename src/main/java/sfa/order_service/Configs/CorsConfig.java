614d97477f44286c76f7378b942b540ada0d6175
kamini <patidarkamini05@gmail.com>
2025-06-18 12:00:05 +0530

    update cors

--- src/main/java/sfa/order_service/Configs/CorsConfig.java
+++ src/main/java/sfa/order_service/Configs/CorsConfig.java
@@ -22,7 +22,10 @@ public class CorsConfig implements WebMvcConfigurer {
                         "https://test-pharma.prism-sfa-dev.net/",
                         "https://www.test-pharma.prism-sfa-dev.net/",
                         "https://test-fmcg.prism-sfa-dev.net/",
-                        "https://www.test-fmcg.prism-sfa-dev.net/"
+                        "https://www.test-fmcg.prism-sfa-dev.net/",
+                        "https://jyoti-pharma.prism-sfa-dev.net/",
+                        "https://sharma-store.prism-sfa-dev.net/",
+                        "https://www.prism-sfa-dev.net/"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
